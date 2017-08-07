d7ceb1d23572fe9337dc8bff5a55bb674f2d9bc8
Stephen Landis <stephen.n.landis@gmail.com>
2017-08-07 15:29:05 -0500

    2-stringReapeat complete

--- src/2-stringRepeatify.js
+++ src/2-stringRepeatify.js
@@ -2,3 +2,14 @@
     Define a repeatify function on the String object.
     'hello'.repeatify(3) should equal 'hellohellohello'
  */
+ String.prototype.repeatify = function (repeat) {
+	var repeatedString = '';
+
+  for (var i =0; i < repeat; i++) {
+  	repeatedString += this.toString();
+  }
+
+	return repeatedString;
+}
+
+console.log('hello'.repeatify(3));
