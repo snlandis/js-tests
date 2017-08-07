e4c71d0049ee02b38af0cea7d9ce0553b094cf81
Stephen Landis <stephen.n.landis@gmail.com>
2017-08-07 15:22:22 -0500

    1-add.js complete

--- src/1-add.js
+++ src/1-add.js
@@ -4,3 +4,54 @@
     window.add(5)(8) to equal 13
     window.add(5, 0) to equal 5
  */
+
+ function add(a,b){
+         var num = function (b){return a+b;};
+         if(typeof b =='undefined'){
+             return num;
+         }else{
+             return num(b);
+         }
+     }
+
+ add(5,6);
+ add(5)(8);
+ add(5,0);
+
+
+
+
+ //old code
+ // var x = 5;
+
+ // var add = function(x) {
+ //   var x = 5;
+ //     return function(y) { return x + y
+ //     ;
+ //   };
+
+ // };
+ // var counter = 5;
+ // var six = 6;
+ // var eight = 8;
+ // var zero = 0;
+
+ // function add() {
+ // var counter = 5;
+ // var six = 6;
+ //     counter + six;
+ //     return counter;
+ // }
+
+ // function add(x) {
+ //     return function(y) {
+ //         if (typeof y !== 'undefined') {
+ //             x = x + y;
+ //             return arguments.callee;
+ //         } else {
+ //             return x;
+ //         }
+ //     };
+ // }
+ // add(1)(2)(3)();
+ // add(1)(1)(1)(1)(1)(1)();
